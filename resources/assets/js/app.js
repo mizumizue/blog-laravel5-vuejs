18234944737fa8c46532a9a35a4280ac0675cce7
kenta-mizushima <ken.water8955@gmail.com>
2018-05-28 23:47:27 +0900

    add Blog Index.vue, Header.vue, vue.js&vue-router

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,22 +1,23 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
 
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
+require('./bootstrap')
 
-require('./bootstrap');
+Vue.use(VueRouter)
 
-window.Vue = require('vue');
+Vue.component('header-component', require('./components/Layouts/Header.vue'))
 
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
+Vue.prototype.$http = window.axios
 
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
+const router = new VueRouter({
+    mode: 'history',
+    routes: [
+        { path: '/', component: require('./components/Layouts/Index.vue') },
+        // { path: '/article/:id', component: require('./components/Layouts/Detail.vue') },
+    ]
+})
 
 const app = new Vue({
-    el: '#app'
-});
+    router,
+    el: '#vueContent'
+})
---
+++ resources/assets/js/components/Layouts/Header.vue
@@ -0,0 +1,8 @@
+<template>
+    <div id="vueHeader">
+        <h1>
+            <router-link to="/">Take IT Easy</router-link>
+        </h1>
+        <p>肩肘張らずにやるブログです</p>
+    </div>
+</template>
