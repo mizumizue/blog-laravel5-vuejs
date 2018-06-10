9bfecbf3a8727231673c4214b744b706a68202f0
kenta-mizushima <ken.water8955@gmail.com>
2018-06-10 19:45:31 +0900

    add admin_menus migration

---
+++ database/migrations/2018_06_10_093857_create_admin_menus_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAdminMenusTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('admin_menus', function (Blueprint $table) {
+            $table->uuid('id');
+            $table->string('title')->nullable(false);
+            $table->string('code')->nullable(false);
+            $table->integer('order')->nullable(true);
+            $table->boolean('visible')->nullable(false);
+            $table->timestamps();
+            $table->primary('id');
+            $table->charset = 'utf8mb4';
+            $table->collation = 'utf8mb4_unicode_ci';
+            $table->engine = 'InnoDB';
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('admin_menus');
+    }
+}
