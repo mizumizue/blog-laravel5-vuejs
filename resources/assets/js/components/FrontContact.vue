f46177566c81ad2f2abc62d8ace2a56ee2846649
kenta-mizushima <ken.water8955@gmail.com>
2018-08-27 22:04:27 +0900

    fix Profile & Contact

--- resources/assets/js/components/FrontContact.vue
+++ resources/assets/js/components/FrontContact.vue
@@ -3,6 +3,15 @@
     <div class="p-md-3 col-9 col-sm-9 col-md-9 mx-auto">
         <h2>コンタクト</h2>
         <hr>
+        <p>仕事の請負などは今のところやっておりません。</p>
+        <p>
+            御用の方は
+            <a href="mailto:trewanek@gmail.com">メール</a>
+            もしくは
+            <a href="https://twitter.com/Trewa_nek">Twitter</a>
+            迄お願いいたします。
+        </p>
+        <p>記事に対するフィードバックも歓迎です。</p>
     </div>
 </div>
 </template>
